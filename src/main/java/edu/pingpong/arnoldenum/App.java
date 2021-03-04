40072367b047ee3fbf9d28cc690fdce7ef7ad69f
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2021-03-04 12:03:48 +0100

    test: :white_check_mark: Changes on test, added new streams()

--- src/main/java/edu/pingpong/arnoldenum/App.java
+++ src/main/java/edu/pingpong/arnoldenum/App.java
@@ -1,5 +1,7 @@
 package edu.pingpong.arnoldenum;
 
+import java.util.*;
+
 import edu.pingpong.arnoldenum.domain.*;
 /**
  * Hello world!
@@ -23,6 +25,25 @@ public class App
         System.out.println("\nYour weight only on the gas giant planets: ");
         for(Planeta planeta: Planeta.getGigantesGaseosos()){
             System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+        }
+
+
+        
+
+
+        String[] gigantesGaseosos = new String[4];
+
+        Arrays.asList(Planeta.values()).subList(4, 8).stream().forEach(p -> gigantesGaseosos[p.ordinal()-4] = p.name());
+
+		// Tuve que realizar Casting porque count() retornaba un LONG
+		int planetasIncluidos = (int) Arrays.asList(Planeta.values()).subList(4, 8).stream().peek(System.out::println).count();
+
+        System.out.println(planetasIncluidos);
+
+		
+		for(Planeta planeta : Planeta.getGigantesGaseosos()){
+			System.out.println(planeta);
+
         }
     }
 }
